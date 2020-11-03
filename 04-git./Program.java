0cc1d5bb0c8b3fac15970f390504043ff6e12178
System Administrator <root@MacBook-Pro-Justyna.local>
2020-11-03 13:27:39 +0100

    com 6

--- 04-git./Program.java
+++ 04-git./Program.java
@@ -1,10 +1,13 @@
 public class MyClass {
   int x = 5;
+  int y = 7;
 
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
     System.out.println(myObj.x);
+    System.out.println(myObj.y);
   }
 }
 This project is a work in progress.
 New commit in new branch
+New code in this file
